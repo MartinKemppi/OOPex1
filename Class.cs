3c9b20161ee4928c00bc262470f05fd666ad9a0e
MartinKemppi <120181210+MartinKemppi@users.noreply.github.com>
2023-09-01 10:54:42 +0300

    Add files via upload
    Updated ver

--- Class.cs
+++ Class.cs
@@ -20,6 +20,38 @@ namespace OOPex1
                 //Console.WriteLine(student.GetCity());
 
                 student.ShowInfo();
+
+                Console.WriteLine();
+
+                Message m1 = new Message("Hello", "John", DateTime.Now.AddDays(-10));
+                Console.WriteLine(m1.Content);
+                Console.WriteLine(m1.Time);
+                m1.AddLike();
+                Console.WriteLine(m1.GetPopularity());
+                               
+                Message m2 = new Message("Hi", "Mary", DateTime.Now.AddMinutes(-1));
+                Console.WriteLine(m2.Content);
+                m2.AddLike();
+                Console.WriteLine(m2.GetPopularity());
+                Console.Write("m1.GetPopularity() < m2.GetPopularity(): ");
+                Console.WriteLine(m1.GetPopularity() < m2.GetPopularity());
+
+
+                m1.MessagePopularity();
+                
+                for (int i = 0; i < 1000; i++) { m2.AddLike(); }
+
+                Console.WriteLine(m2.GetPopularity());
+                Console.WriteLine(m1.GetPopularityInfo(m1.GetPopularity(), m2.GetPopularity()));
+                Message m3 = new Message("Tere","Mari",DateTime.Now.AddDays(-1));
+                Message m4 = new Message("Guten Tag", "Hans", DateTime.Now.AddDays(-5));
+                Message m5 = new Message("Hei", "Marika", DateTime.Now.AddDays(-10));
+                List<Message> list = new List<Message>();
+                list.Add(m1);
+                list.Add(m2);
+                list.Add(m3);
+                list.Add(m4);
+                list.Add(m5);
             }
         }
          
--- Student.cs
+++ Student.cs
@@ -60,4 +60,119 @@ namespace OOPex1
 
         }
     }
+
+    public class Message
+    {
+        private readonly string _content;
+        private readonly string _author;
+        private readonly DateTime _time;
+        private int _likes;
+
+        public Message(string content, string author, DateTime time)
+        {
+            this._content = content;
+            this._author = author;
+            this._time = time;
+        }
+
+        public int Likes { get => _likes; }
+        public DateTime Time { get => _time; }
+        public string Author { get => _author; }
+        public string Content { get => _content; }
+
+        public void AddLike()
+        {
+            _likes++;
+        }
+
+        public double GetPopularity()
+        {
+            double elapsed = DateTime.Now.Subtract(this._time).TotalSeconds;
+            if (elapsed == 0)
+            {
+                return _likes;
+            }
+            return _likes / elapsed;
+
+        }
+
+        public void MessagePopularity(/*string content, string author, DateTime time*/)
+        {
+            int paljusõnumi = Convert.ToInt32(Console.ReadLine()); //N message - peab teha universaalne arv täitmiseks
+            for (int i = 0; i < paljusõnumi; i++)
+            {
+                Console.WriteLine("Sisesta sõnum: ");
+                string _content = Console.ReadLine();
+                Console.WriteLine("Sisesta autor: ");
+                string _author = Console.ReadLine();
+                Console.WriteLine("Palju aega läbis sõnumit? paev, tund või minutt");
+                string _vastus = Console.ReadLine();
+                int _time = Convert.ToInt32(Console.ReadLine());
+
+                    if (_time > 0)
+                    {
+                        _time *= -1;
+                    }
+                        if (_vastus == "paev")
+                        {
+                            Message m1 = new Message(_content, _author, DateTime.Now.AddDays(_time));
+                            Console.WriteLine(m1.Content);
+                            Console.WriteLine(m1.Time);
+                            m1.AddLike();
+                            Console.WriteLine(m1.GetPopularity());
+                        }
+                        else if (_vastus == "tund")
+                        {
+                            Message m1 = new Message(_content, _author, DateTime.Now.AddHours(_time));
+                            Console.WriteLine(m1.Content);
+                            Console.WriteLine(m1.Time);
+                            m1.AddLike();
+                            Console.WriteLine(m1.GetPopularity());
+                        }
+                        else if (_vastus == "minutt")
+                        {
+                            Message m1 = new Message(_content, _author, DateTime.Now.AddMinutes(_time));
+                            Console.WriteLine(m1.Content);
+                            Console.WriteLine(m1.Time);
+                            m1.AddLike();
+                            Console.WriteLine(m1.GetPopularity());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Kijuta õige aja tüüp: paev, tund või minutt");
+                            Console.ReadLine();
+                        }
+            }
+        }
+
+        public string Compare(double m1, double m2)
+        {
+            string result = "";
+            if(m1 > m2) 
+            {
+                result = "m1 sõnum on populaarsem kui m2";
+            }
+            if(m1 < m2) 
+            {
+                result = "m2 sõnum on populaarsem kui m1";
+            }
+            return result;
+        }
+
+        public string GetPopularityInfo(List<Message>messages)
+        {
+            string result = "";
+            double popularity = 0;
+            for (int i= 0; i < messages.Count; i++) 
+            {
+                if (messages[i].GetPopularity()>popularity)
+                {
+                    result = messages[i].Content + " on kõige populaarsem sõnum, seda kirjutas " + messages[i].Author;
+                }
+            }
+
+            return result;
+
+        }
+    }
 }
